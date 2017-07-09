a57757334717df570fd4fd4e2f3c90efdfaf46d2
Richard Cornish <rcornish05@gmail.com>
2017-07-08 20:18:16 -0400

    .

--- Object Slicing and Polymorphism/Object Slicing and Polymorphism.cpp
+++ Object Slicing and Polymorphism/Object Slicing and Polymorphism.cpp
@@ -9,50 +9,49 @@
 
 class Parent {
 private:
-	int one;
+  int one;
 
 public:
-	Parent(): one(0) {}
+  Parent(): one(0) {}
 
-	Parent(const Parent &other ): one(0) {
-		one = other.one;
-		std::cout << "Parent copy constructor" << std::endl;
-	}
+  Parent(const Parent &other ): one(0) {
+    one = other.one;
+    std::cout << "Parent copy constructor" << std::endl;
+  }
 
-	virtual void print() {
-		std::cout << "parent" << std::endl;
-	}
+  virtual void print() {
+    std::cout << "parent" << std::endl;
+  }
 
-	virtual ~Parent() { }
+  virtual ~Parent() { }
 };
 
 class Child : public Parent {
 private:
-	int two;
+  int two;
 public:
-	Child(): two() { }
-	void print() {
-		std::cout << "child" << std::endl;
-	}
+  Child(): two() { }
+  void print() {
+    std::cout << "child" << std::endl;
+  }
 };
 
 int main( int argc, char** argv ) {
 
-	Child c1;
-	Parent *p1 = &c1;
+  Child c1;
+  Parent *p1 = &c1;
 
-	p1->print();
+  p1->print();
 
+  /* Object slicing happens when an object of the Parent type is instantiated and initialized
+   * with a value of a child type. In the below example p2 is copy constructed. Inside the Parent
+   * copy constructor member one is initialized, but
+   */
 
-	/* Object slicing happens when an object of the Parent type is instantiated and initialized
-	 * with a value of a child type. In the below example p2 is copy constructed. Inside the Parent
-	 * copy constructor member one is initialized, but
-	*
-	*/
-	Parent p2 = Child();
-	p2.print();
+  Parent p2 = Child();
+  p2.print();
 
-	return 0;
+  return 0;
 }
 
 
